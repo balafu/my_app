33bdbfa05f13a798d278a857d2507d16e8afc670
Balazs Fuge <Balazs_Fuge@epam.com>
2016-03-29 12:11:28 +0200

    import project

---
+++ src/main/java/my_test_package/App.java
@@ -0,0 +1,17 @@
+package my_test_package;
+
+import org.slf4j.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+	Logger logger = LoggerFactory.getLogger(App.class);
+	logger.info("Hello World!");
+    }
+}
